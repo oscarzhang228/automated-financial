24a40cf1a5995eeef1d98d6db8cc36ecac80d632
riyau8 <upadhyayariya@gmail.com>
2024-02-26 22:08:17 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Budget from "./pages/Budget";
 import Transactions from "./pages/Transactions";
-import Login from "./pages/Login";
-import Navbar from "./components/Navbar";
 
+import Testing from "./pages/Testing";
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/budget" element={<Budget />} />
         <Route path="/transactions" element={<Transactions />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/api-test" element={<Testing />} />
       </Routes>
     </Router>
   );
